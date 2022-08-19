33d62ceb035f47768fb39e90f3e3065a860b4107
Nathálya Ricci <nathalyaricci@gmail.com>
2022-08-19 18:53:47 -0300

    teste dark mode

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,3 +1,15 @@
-function darkMode() {
-  let 
+let btn = document.getElementById('btn-toggle');
+
+let currentTheme = localStorage.getItem('theme-mode');
+
+if (currentTheme == 'dark') {
+	document.body.classList.toggle('dark-mode');
+} else if (currentTheme == 'light') {
+	document.body.classList.toggle('light-mode');
 }
+
+btn.addEventListener('click', function () {
+	document.body.classList.toggle('dark-mode');
+	let theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+	localStorage.setItem('theme-mode', theme);
+});
